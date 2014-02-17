e0941f05703a4243c3f89720c3b889ae97d1cd10
lpelias <liunerd@outlook.com>
2014-02-17 18:59:34 +0800

    Update BestTimetoBuyandSellStockIII.cpp

--- BestTimetoBuyandSellStockIII/BestTimetoBuyandSellStockIII.cpp
+++ BestTimetoBuyandSellStockIII/BestTimetoBuyandSellStockIII.cpp
@@ -4,9 +4,8 @@ public:
         int len = prices.size();
         if(len < 2) return 0;
         int* max_forward = new int[len];
-        int* max_backward = new int[len];
-        int i = 0, low = prices[0], max_profit = 0, curr_profit;
-        max_forward[0] = max_backward[len - 1] = 0;
+        int i = 0, low = prices[0], max_profit = 0, curr_profit = 0;
+        max_forward[0] = 0;
         while(++i < len){
         	if(prices[i] < low){
         		low = prices[i];
@@ -19,30 +18,20 @@ public:
         	max_forward[i] = max_profit;
         }
         i = len - 1;
-        low = 0 - prices[len-1];
-        max_profit = 0;
+        low = prices[len-1];
+        max_profit = max_forward[len-1];
         while(--i >= 0){
-        	int val = 0 - prices[i];
-        	if(val < low){
-        		low = val;
+        	if(prices[i] > low){
+        		low = prices[i];
         	}else{
-        	    curr_profit = val - low;
+        	    curr_profit = low - prices[i];
+        		curr_profit += max_forward[i];
         		if(curr_profit > max_profit){
         			max_profit = curr_profit;
         		}
         	}
-        	max_backward[i] = max_profit;
-        }
-        max_profit = 0;
-        i = 0;
-        for(i = 0; i < len; ++i){
-            curr_profit = max_forward[i] + max_backward[i];
-        	if(curr_profit > max_profit){
-        		max_profit = curr_profit;
-        	}
         }
         delete[] max_forward;
-        delete[] max_backward;
         return max_profit;
     }
 };
